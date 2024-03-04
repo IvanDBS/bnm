9190a738a4f01953b4f2a91fd5bfe0f7c1932898
Ivan Teaca <ivan.teaca@gmail.com>
2024-03-04 10:48:11 +0200

    Rubocop

--- main.rb
+++ main.rb
@@ -1,35 +1,39 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'nokogiri'
 require 'telegram/bot'
 require 'date'
 
 def get_exchange_rates(date)
+  data = fetch_exchange_data(date)
+  parse_exchange_rates(data)
+end
+
+def fetch_exchange_data(date)
   url = URI.parse("https://bnm.md/en/official_exchange_rates?get_xml=1&date=#{date}")
   response = Net::HTTP.get_response(url)
-  data = response.body
+  response.body
+end
 
+def parse_exchange_rates(data)
   doc = Nokogiri::XML(data)
 
-  # Извлечение информации о курсах валют
-  # Пример: извлечение курсов евро, доллара, гривны, лея и рубля
-  euro_rate = doc.at_xpath('//Valute[@ID="47"]/Value')&.content
-  usd_rate = doc.at_xpath('//Valute[@ID="44"]/Value')&.content
-  uah_rate = doc.at_xpath('//Valute[@ID="53"]/Value')&.content  # Исправлен ID для гривны
-  ron_rate = doc.at_xpath('//Valute[@ID="49"]/Value')&.content
-  rub_rate = doc.at_xpath('//Valute[@ID="51"]/Value')&.content  # Исправлен ID для рубля
-
-  # Возвращаем хэш с данными о курсах валют
   {
-    'euro' => euro_rate,
-    'usd' => usd_rate,
-    'uah' => uah_rate,
-    'ron' => ron_rate,
-    'rub' => rub_rate
+    'euro' => extract_rate(doc, '47'),
+    'usd' => extract_rate(doc, '44'),
+    'uah' => extract_rate(doc, '53'), # Updated ID for UAH
+    'ron' => extract_rate(doc, '49'),
+    'rub' => extract_rate(doc, '51') # Updated ID for RUB
   }
 end
 
-# Инициализация телеграм-бота
-token = '6716806477:AAEsJdC1I7wO1SRJm2AgHAeahlUqvR0nasg'
+def extract_rate(doc, valute_id)
+  doc.at_xpath("//Valute[@ID='#{valute_id}']/Value")&.content
+end
+
+# Initializing the Telegram bot
+token = 'YOUR_TELEGRAM_BOT_TOKEN'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
@@ -37,7 +41,7 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: 'Salut! Pentru afisarea cursului apasa Menu!')
     when '/get_rates'
       current_date = Date.today
-      # Если сегодня суббота или воскресенье, то находим последний рабочий день (пятницу)
+      # If today is Saturday or Sunday, find the last working day (Friday)
       if current_date.saturday?
         current_date -= 1
       elsif current_date.sunday?
@@ -47,15 +51,15 @@ Telegram::Bot::Client.run(token) do |bot|
       formatted_date = current_date.strftime('%d.%m.%Y')
       rates = get_exchange_rates(formatted_date)
 
-      # Формируем текст для отправки в боте
+      # Generating text to send in the bot
       rates_text = "Curs valutar BNM, #{formatted_date}:\n"
       rates_text += "EUR: #{rates['euro']} MDL\n"
       rates_text += "USD: #{rates['usd']} MDL\n"
       rates_text += "UAH: #{rates['uah']} MDL\n"
       rates_text += "RON: #{rates['ron']} MDL\n"
       rates_text += "RUB: #{rates['rub']} MDL\n"
-      rates_text += "Să aveți o zi productivă în continuare!"
-      # Отправка информации о курсах валют
+      rates_text += 'Să aveți o zi productivă în continuare!'
+      # Sending information about exchange rates
       bot.api.send_message(chat_id: message.chat.id, text: rates_text)
     end
   end
